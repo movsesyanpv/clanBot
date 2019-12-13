74d217b1b4a8e35147164fb2c952d130bc47c9d7
Pavel Movsesyan <movsesyanpv@users.noreply.github.com>
2019-12-13 18:38:02 +0300

    Update decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$api" \
---output ../../api.json ../../api.json.gpg
+--output $HOME/api.json $HOME/api.json.gpg
