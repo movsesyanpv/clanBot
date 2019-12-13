b84db8e79681b525a2d7ac1112e37f21ecfe23fa
movsesyanpv <movsesyanpv1995@live.com>
2019-12-13 19:23:44 +0300

    Added encrypt_secret.sh

---
+++ .github/scripts/encrypt_secret.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Decrypt the file
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --passphrase "$api" --batch --symmetric --cipher-algo AES256 ../../translations.json
