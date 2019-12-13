2c3b5361b825896b1951f6af155c92a51f7dbff2
movsesyanpv <movsesyanpv1995@live.com>
2019-12-13 18:59:48 +0300

    Updated workflow

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,3 +4,12 @@
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$api" \
 --output ./api.json ./api.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$api" \
+--output ./char.json ./char.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$api" \
+--output ./channelList.dat ./channelList.dat
+
+gpg --quiet --batch --yes --decrypt --passphrase="$api" \
+--output ./token.json ./token.json.gpg
