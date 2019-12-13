7d49905a6ec85bb8d1eb7cc03069c8ab8c84be81
Pavel Movsesyan <movsesyanpv@users.noreply.github.com>
2019-12-13 19:26:14 +0300

    Update encrypt_secret.sh

--- .github/scripts/encrypt_secret.sh
+++ .github/scripts/encrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --passphrase "$api" --batch --symmetric --cipher-algo AES256 ../../translations.json
+gpg --passphrase "$api" --batch --symmetric --cipher-algo AES256 ./translations.json
