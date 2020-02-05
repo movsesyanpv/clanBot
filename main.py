2baa320d6c4f5d16cc63bb9bb0bb57b57ae35c66
movsesyanpv <movsesyanpv1995@live.com>
2020-02-05 15:57:43 +0300

    Added some exception notifications

--- main.py
+++ main.py
@@ -150,58 +150,65 @@ class ClanBot(discord.Client):
         if payload.user_id == self.user.id:
             return
 
-        message = await self.fetch_channel(payload.channel_id)
-        message = await message.fetch_message(payload.message_id)
-        for guild in self.guilds:
-            if guild.id == payload.guild_id:
-                user = guild.get_member(payload.user_id)
+        try:
+            message = await self.fetch_channel(payload.channel_id)
+            message = await message.fetch_message(payload.message_id)
+            for guild in self.guilds:
+                if guild.id == payload.guild_id:
+                    user = guild.get_member(payload.user_id)
 
-        if self.raid.is_raid(message.id):
-            mode = self.raid.get_cell('group_id', message.id, 'group_mode')
-            owner = self.get_user(self.raid.get_cell('group_id', message.id, 'owner'))
-            if str(payload.emoji) == '❌' and payload.user_id == owner.id:
-                if mode == 'manual':
-                    dm_id = self.raid.get_cell('group_id', message.id, 'dm_message')
-                    if owner.dm_channel is None:
-                        await owner.create_dm()
-                    if dm_id != 0:
-                        dm_message = await owner.dm_channel.fetch_message(dm_id)
-                        await dm_message.delete()
-                self.raid.del_entry(message.id)
-                await message.delete()
-                return
-            if str(payload.emoji) != '👌':
-                for reaction in message.reactions:
-                    if str(reaction.emoji) == str(payload.emoji):
-                        await reaction.remove(user)
-                        return
-            owner = self.raid.get_cell('group_id', message.id, 'owner')
-            owner = self.get_user(owner)
-            self.raid.add_people(message.id, user)
-            if user.dm_channel is None:
-                await user.create_dm()
-            if mode == 'manual':
-                await user.dm_channel.send(self.translations[self.args.lang]['lfg']['gotcha'], delete_after=30)
-                await self.raid.upd_dm(owner, message.id, self.translations[self.args.lang])
-            if mode == 'basic':
-                await self.raid.update_group_msg(message, self.translations[self.args.lang])
-            return
-
-        raid_dm = self.raid.get_cell('dm_message', message.id, 'dm_message')
-
-        if raid_dm == message.id:
-            emojis = ["{}\N{COMBINING ENCLOSING KEYCAP}".format(num) for num in range(1, 7)]
-            if str(payload.emoji) in emojis:
-                number = emojis.index(str(payload.emoji))
-                lfg_message = self.raid.get_cell('dm_message', raid_dm, 'group_id')
-                channel = self.raid.get_cell('group_id', lfg_message, 'lfg_channel')
-                message = await self.fetch_channel(channel)
-                message = await message.fetch_message(lfg_message)
-                await self.raid.add_going(lfg_message, number)
-                await self.raid.update_group_msg(message, self.translations[self.args.lang])
+            if self.raid.is_raid(message.id):
+                mode = self.raid.get_cell('group_id', message.id, 'group_mode')
+                owner = self.get_user(self.raid.get_cell('group_id', message.id, 'owner'))
+                if str(payload.emoji) == '❌' and payload.user_id == owner.id:
+                    if mode == 'manual':
+                        dm_id = self.raid.get_cell('group_id', message.id, 'dm_message')
+                        if owner.dm_channel is None:
+                            await owner.create_dm()
+                        if dm_id != 0:
+                            dm_message = await owner.dm_channel.fetch_message(dm_id)
+                            await dm_message.delete()
+                    self.raid.del_entry(message.id)
+                    await message.delete()
+                    return
+                if str(payload.emoji) != '👌':
+                    for reaction in message.reactions:
+                        if str(reaction.emoji) == str(payload.emoji):
+                            await reaction.remove(user)
+                            return
                 owner = self.raid.get_cell('group_id', message.id, 'owner')
                 owner = self.get_user(owner)
-                await self.raid.upd_dm(owner, lfg_message, self.translations[self.args.lang])
+                self.raid.add_people(message.id, user)
+                if user.dm_channel is None:
+                    await user.create_dm()
+                if mode == 'manual':
+                    await user.dm_channel.send(self.translations[self.args.lang]['lfg']['gotcha'], delete_after=30)
+                    await self.raid.upd_dm(owner, message.id, self.translations[self.args.lang])
+                if mode == 'basic':
+                    await self.raid.update_group_msg(message, self.translations[self.args.lang])
+                return
+
+            raid_dm = self.raid.get_cell('dm_message', message.id, 'dm_message')
+
+            if raid_dm == message.id:
+                emojis = ["{}\N{COMBINING ENCLOSING KEYCAP}".format(num) for num in range(1, 7)]
+                if str(payload.emoji) in emojis:
+                    number = emojis.index(str(payload.emoji))
+                    lfg_message = self.raid.get_cell('dm_message', raid_dm, 'group_id')
+                    channel = self.raid.get_cell('group_id', lfg_message, 'lfg_channel')
+                    message = await self.fetch_channel(channel)
+                    message = await message.fetch_message(lfg_message)
+                    await self.raid.add_going(lfg_message, number)
+                    await self.raid.update_group_msg(message, self.translations[self.args.lang])
+                    owner = self.raid.get_cell('group_id', message.id, 'owner')
+                    owner = self.get_user(owner)
+                    await self.raid.upd_dm(owner, lfg_message, self.translations[self.args.lang])
+        except Exception as e:
+            bot_info = await self.application_info()
+            owner = bot_info.owner
+            if owner.dm_channel is None:
+                await owner.create_dm()
+            await owner.dm_channel.send('`{}`'.format(traceback.format_exc()))
 
     async def pause_for(self, message, delta):
         self.sched.pause()
@@ -226,99 +233,113 @@ class ClanBot(discord.Client):
         if message.author == self.user:
             return
 
-        if 'lfglist' in message.content.lower() and str(message.channel.type) == 'private':
-            await self.raid.dm_lfgs(message.author)
-            return
+        try:
+            if 'lfglist' in message.content.lower() and str(message.channel.type) == 'private':
+                await self.raid.dm_lfgs(message.author)
+                return
 
-        if 'stop' in message.content.lower() and (self.user in message.mentions or str(message.channel.type) == 'private'):
-            if await self.check_ownership(message):
-                msg = 'Ok, {}'.format(message.author.mention)
+            if 'stop' in message.content.lower() and (self.user in message.mentions or str(message.channel.type) == 'private'):
+                if await self.check_ownership(message):
+                    msg = 'Ok, {}'.format(message.author.mention)
+                    for i in self.emojis:
+                        msg = '{} {}'.format(msg, i)
+                    await message.channel.send(msg)
+                    self.sched.shutdown(wait=True)
+                    await self.logout()
+                    await self.close()
+                    return
+                return
+
+            if 'plan maintenance' in message.content.lower() and (self.user in message.mentions or str(message.channel.type) == 'private'):
+                if await self.check_ownership(message):
+                    try:
+                        content = message.content.splitlines()
+                        start = datetime.strptime(content[1], "%d-%m-%Y %H:%M %z")
+                        finish = datetime.strptime(content[2], "%d-%m-%Y %H:%M %z")
+                        delta = finish-start
+                        self.sched.add_job(self.pause_for, 'date', run_date=start, args=[message, delta], misfire_grace_time=600)
+                    except Exception as e:
+                        await message.channel.send('exception `{}`\nUse following format:```plan maintenance\n'
+                                                   '<start time formatted %d-%m-%Y %H:%M %z>\n'
+                                                   '<finish time formatted %d-%m-%Y %H:%M %z>```'.format(str(e)))
+                    return
+                return
+
+            if str(message.channel.type) == 'private':
+                msg = 'Can\'t do this a private chat, {}'.format(message.author.mention)
                 await message.channel.send(msg)
-                self.sched.shutdown(wait=True)
-                await self.logout()
-                await self.close()
                 return
-            return
 
-        if 'plan maintenance' in message.content.lower() and (self.user in message.mentions or str(message.channel.type) == 'private'):
-            if await self.check_ownership(message):
-                try:
-                    content = message.content.splitlines()
-                    start = datetime.strptime(content[1], "%d-%m-%Y %H:%M %z")
-                    finish = datetime.strptime(content[2], "%d-%m-%Y %H:%M %z")
-                    delta = finish-start
-                    self.sched.add_job(self.pause_for, 'date', run_date=start, args=[message, delta], misfire_grace_time=600)
-                except Exception as e:
-                    await message.channel.send('exception `{}`\nUse following format:```plan maintenance\n'
-                                               '<start time formatted %d-%m-%Y %H:%M %z>\n'
-                                               '<finish time formatted %d-%m-%Y %H:%M %z>```'.format(str(e)))
+            if 'edit lfg' in message.content.lower() and self.user in message.mentions:
+                text = message.content.split()
+                hashids = Hashids()
+                for word in text:
+                    group_id = hashids.decode(word)
+                    if len(group_id) > 0:
+                        old_lfg = self.raid.get_cell('group_id', group_id[0], 'lfg_channel')
+                        old_lfg = self.get_channel(old_lfg)
+                        old_lfg = await old_lfg.fetch_message(group_id[0])
+                        await self.raid.edit(message, old_lfg, self.translations[self.args.lang])
                 return
-            return
 
-        if str(message.channel.type) == 'private':
-            msg = 'Can\'t do this a private chat, {}'.format(message.author.mention)
-            await message.channel.send(msg)
-            return
-
-        if 'edit lfg' in message.content.lower() and self.user in message.mentions:
-            text = message.content.split()
-            hashids = Hashids()
-            for word in text:
-                group_id = hashids.decode(word)
-                if len(group_id) > 0:
-                    old_lfg = self.raid.get_cell('group_id', group_id[0], 'lfg_channel')
-                    old_lfg = self.get_channel(old_lfg)
-                    old_lfg = await old_lfg.fetch_message(group_id[0])
-                    await self.raid.edit(message, old_lfg, self.translations[self.args.lang])
-            return
-
-        if 'lfg' in message.content.lower().splitlines()[0] and self.user in message.mentions:
-            if '-man' in message.content.lower():
-                await self.send_lfg_man(message.author)
+            if 'lfg' in message.content.lower().splitlines()[0] and self.user in message.mentions:
+                if '-man' in message.content.lower():
+                    await self.send_lfg_man(message.author)
+                    await message.delete()
+                    return
+                self.raid.add(message)
+                role = self.raid.get_cell('group_id', message.id, 'the_role')
+                name = self.raid.get_cell('group_id', message.id, 'name')
+                time = datetime.fromtimestamp(self.raid.get_cell('group_id', message.id, 'time'))
+                is_embed = self.raid.get_cell('group_id', message.id, 'is_embed')
+                description = self.raid.get_cell('group_id', message.id, 'description')
+                msg = "{}, {} {}\n{} {}\n{}".format(role, self.translations[self.args.lang]['lfg']['go'], name,
+                                                    self.translations[self.args.lang]['lfg']['at'], time, description)
+                if is_embed:
+                    embed = self.raid.make_embed(message, self.translations[self.args.lang])
+                    out = await message.channel.send(content=msg)
+                    await out.edit(content=None, embed=embed)
+                else:
+                    out = await message.channel.send(msg)
+                end_time = time + timedelta(seconds=self.raid.get_cell('group_id', message.id, 'length'))
+                await out.add_reaction('👌')
+                await out.add_reaction('❌')
+                self.raid.set_id(out.id, message.id)
+                await self.raid.update_group_msg(out, self.translations[self.args.lang])
+                # self.sched.add_job(out.delete, 'date', run_date=end_time, id='{}_del'.format(out.id))
                 await message.delete()
                 return
-            self.raid.add(message)
-            role = self.raid.get_cell('group_id', message.id, 'the_role')
-            name = self.raid.get_cell('group_id', message.id, 'name')
-            time = datetime.fromtimestamp(self.raid.get_cell('group_id', message.id, 'time'))
-            is_embed = self.raid.get_cell('group_id', message.id, 'is_embed')
-            description = self.raid.get_cell('group_id', message.id, 'description')
-            msg = "{}, {} {}\n{} {}\n{}".format(role, self.translations[self.args.lang]['lfg']['go'], name,
-                                                self.translations[self.args.lang]['lfg']['at'], time, description)
-            if is_embed:
-                embed = self.raid.make_embed(message, self.translations[self.args.lang])
-                out = await message.channel.send(content=msg)
-                await out.edit(content=None, embed=embed)
-            else:
-                out = await message.channel.send(msg)
-            end_time = time + timedelta(seconds=3600)
-            await out.add_reaction('👌')
-            await out.add_reaction('❌')
-            self.raid.set_id(out.id, message.id)
-            # self.sched.add_job(out.delete, 'date', run_date=end_time, id='{}_del'.format(out.id))
-            await message.delete()
-            return
 
-        if 'regnotifier' in message.content.lower() and self.user in message.mentions:
-            if await self.check_ownership(message):
-                await message.delete()
-                self.channels.append(message.channel.id)
-                self.channels = list(set(self.channels))
-                f = open('channelList.dat', 'w')
-                for channel in self.channels:
-                    f.write('{}\n'.format(channel))
-                f.close()
-                msg = 'Got it, {}'.format(message.author.mention)
-                await message.channel.send(msg, delete_after=10)
+            if 'regnotifier' in message.content.lower() and self.user in message.mentions:
+                if await self.check_ownership(message):
+                    await message.delete()
+                    self.channels.append(message.channel.id)
+                    self.channels = list(set(self.channels))
+                    f = open('channelList.dat', 'w')
+                    for channel in self.channels:
+                        f.write('{}\n'.format(channel))
+                    f.close()
+                    msg = 'Got it, {}'.format(message.author.mention)
+                    await message.channel.send(msg, delete_after=10)
+                    return
                 return
-            return
 
-        if 'update' in message.content.lower() and self.user in message.mentions:
-            content = message.content.lower().split()
-            await message.delete()
-            for upd_type in content[2:]:
-                await self.force_update(upd_type)
-            return
+            if 'update' in message.content.lower() and self.user in message.mentions:
+                content = message.content.lower().split()
+                await message.delete()
+                for upd_type in content[2:]:
+                    await self.force_update(upd_type)
+                return
+        except Exception as e:
+            bot_info = await self.application_info()
+            owner = bot_info.owner
+            if owner.dm_channel is None:
+                await owner.create_dm()
+            await owner.dm_channel.send('`{}`'.format(traceback.format_exc()))
+            await owner.dm_channel.send('{}:\n{}'.format(message.author, message.content))
+            if message.author.dm_channel is None:
+                await message.author.create_dm()
+            await message.author.dm_channel.send(self.translations[self.args.lang]['msg']['error'])
 
     def get_channels(self):
         try:
