b850039c90a2c439a921ddfedb01eadf1b03d4e6
movsesyanpv <movsesyanpv1995@live.com>
2020-02-14 19:08:57 +0300

    Added Gandalf!

--- unauthorized.py
+++ unauthorized.py
@@ -9,6 +9,7 @@ def get_unauth_response():
         c = conn.cursor()
         e = c.execute('''SELECT embed FROM \'401responses\' ORDER BY RANDOM() LIMIT 1;''').fetchone()
         e = discord.Embed.from_dict(json.loads(e[0]))
+        conn.close()
     except sqlite3.OperationalError:
         e = discord.Embed(title='I will not obey you.', type="rich",
                           url='https://www.youtube.com/watch?v=qn9FkoqYgI4')
