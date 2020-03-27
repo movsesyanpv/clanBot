ab5bc4348ef3b4a8289038a0a00ead360758f059
movsesyanpv <movsesyanpv1995@live.com>
2020-03-27 14:20:34 +0300

    Added maybe_goers to updater

--- updater.py
+++ updater.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 
 def go():
@@ -7,4 +8,11 @@ def go():
 
 
 def migrate_db():
-    pass
+    lfg_db = sqlite3.connect('lfg.db')
+    lfg_cursor = lfg_db.cursor()
+
+    try:
+        lfg_cursor.execute('''ALTER TABLE raid ADD COLUMN maybe_goers text''')
+        lfg_db.commit()
+    except sqlite3.OperationalError:
+        pass
