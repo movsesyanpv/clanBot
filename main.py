6106bdc3310fbdd4f864806071e695e93d614ac8
movsesyanpv <movsesyanpv1995@live.com>
2020-03-27 14:12:33 +0300

    Bump version number

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import unauthorized
 
 
 class ClanBot(commands.Bot):
-    version = '2.11.3'
+    version = '2.11.4'
     cog_list = ['cogs.admin', 'cogs.updates', 'cogs.group']
     langs = ['en', 'ru']
     all_types = ['weekly', 'daily', 'spider', 'xur', 'tess', 'seasonal']
