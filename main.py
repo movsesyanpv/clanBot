e0d5e421d2039a3c2175f110d3b3d14181545e57
movsesyanpv <movsesyanpv1995@live.com>
2020-10-19 15:53:25 +0300

    Fix #238
    fix #239
    fix #240
    fix #241
    fix #242
    fix #243
    fix #244
    fix #245

--- main.py
+++ main.py
@@ -500,7 +500,8 @@ class ClanBot(commands.Bot):
             #         return
             #     raise exception
             elif isinstance(exception, discord.errors.Forbidden):
-                pass
+                if isinstance(exception.original, discord.errors.Forbidden):
+                    pass
             else:
                 if 'stop' not in message.content.lower() or (
                         self.user not in message.mentions and str(message.channel.type) != 'private'):
