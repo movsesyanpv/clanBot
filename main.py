375714d98d324f5a9c568f695a156538880ffe1d
Pavel Movsesyan <movsesyanpv1995@live.com>
2021-03-03 13:48:15 +0300

    Version bump

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import unauthorized
 
 
 class ClanBot(commands.Bot):
-    version = '2.17.4'
+    version = '2.17.5'
     cog_list = ['cogs.admin', 'cogs.public', 'cogs.group', 'cogs.serveradmin']
     langs = ['de', 'en', 'es', 'es-mx', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-br', 'ru', 'zh-cht']
     all_types = ['weekly', 'nightmares', 'crucible', 'raids', 'ordeal', 'evweekly', 'empire', 'daily', 'strikes', 'spider', 'banshee', 'xur', 'osiris', 'alerts', 'events']
