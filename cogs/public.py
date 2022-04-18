8414e54720c9a76b4e8b8384e66bdf0ce02d08c7
movsesyanpv <movsesyanpv1995@live.com>
2022-04-18 18:36:52 +0300

    Removed obsolete code

--- cogs/public.py
+++ cogs/public.py
@@ -33,127 +33,6 @@ class Public(commands.Cog):
                           invite_label=translations['invite_btn'])
         await ctx.message.reply(embed=response_embed, view=view, mention_author=False)
 
-    # @commands.command(
-    #     aliases=['gtop', 'globaltop']
-    # )
-    # @commands.guild_only()
-    # async def top(self, ctx, metric, number=10):
-    #     ctx.bot.guild_cursor.execute('''SELECT clan_id FROM clans WHERE server_id=?''', (ctx.guild.id,))
-    #     clan_id = ctx.bot.guild_cursor.fetchone()
-    #     lang = ctx.bot.guild_lang(ctx.message.guild.id)
-    #     translations = ctx.bot.translations[lang]['top']
-    #     await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'])
-    #     if ctx.invoked_with in ['gtop', 'globaltop']:
-    #         is_global = True
-    #     else:
-    #         is_global = False
-    #     if clan_id is None:
-    #         clan_id = [0]
-    #     if clan_id[0] == 0:
-    #         await ctx.channel.send(translations['no_clan'], delete_after=60)
-    #         return
-    #     if len(clan_id) > 0:
-    #         clan_ids = [clan_id[0]]
-    #         try:
-    #             int(metric)
-    #             is_time = False
-    #             is_kda = False
-    #         except ValueError:
-    #             try:
-    #                 internal_db = mariadb.connect(host=ctx.bot.api_data['db_host'], user=ctx.bot.api_data['cache_login'],
-    #                                               password=ctx.bot.api_data['pass'], port=ctx.bot.api_data['db_port'],
-    #                                               database='metrics')
-    #                 internal_cursor = internal_db.cursor()
-    #                 internal_cursor.execute('''SELECT hash FROM seasonsmetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM accountmetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM cruciblemetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM destinationmetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM gambitmetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM raidsmetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM strikesmetrics WHERE name=? and is_working=1
-    #                                     UNION ALL
-    #                                     SELECT hash FROM trialsofosirismetrics WHERE name=?  and is_working=1''',
-    #                                         (metric.lower(), metric.lower(),
-    #                                          metric.lower(), metric.lower(),
-    #                                          metric.lower(), metric.lower(),
-    #                                          metric.lower(), metric.lower()))
-    #                 metric_id = internal_cursor.fetchone()
-    #                 if 'kda' in metric.lower():
-    #                     is_kda = True
-    #                 else:
-    #                     is_kda = False
-    #                 if 'speed' in metric.lower():
-    #                     is_time = True
-    #                 else:
-    #                     is_time = False
-    #                 if metric_id is not None:
-    #                     if len(metric_id) > 0:
-    #                         metric = metric_id[0]
-    #                     else:
-    #                         internal_db.close()
-    #                         raise mariadb.Error
-    #                 else:
-    #                     internal_db.close()
-    #                     raise mariadb.Error
-    #                 internal_db.close()
-    #             except mariadb.Error:
-    #                 await ctx.channel.send(translations['unknown_metric'].format(metric), delete_after=10)
-    #                 if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #                     try:
-    #                         await ctx.message.delete()
-    #                     except discord.NotFound:
-    #                         pass
-    #                 return
-    #         try:
-    #             top_name = await ctx.bot.data.destiny.decode_hash(metric, 'DestinyMetricDefinition', language=lang)
-    #         except pydest.pydest.PydestException:
-    #             await ctx.channel.send(translations['unknown_metric'].format(metric), delete_after=10)
-    #             if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #                 await ctx.message.delete()
-    #             return
-    #             await ctx.channel.send(translations['in_progress'], delete_after=30)
-    #         if is_global:
-    #             clan_ids_c = ctx.bot.guild_cursor.execute('''SELECT clan_id FROM clans''')
-    #             clan_ids_c = clan_ids_c.fetchall()
-    #             clan_ids = []
-    #             for clan_id in clan_ids_c:
-    #                 if clan_id[0] not in clan_ids:
-    #                     clan_ids.append(clan_id[0])
-    #         top_list = await ctx.bot.data.get_clan_leaderboard(clan_ids, metric, number, is_time, is_kda, is_global)
-    #         max_len = min(number, len(top_list))
-    #         if len(top_list) > 0:
-    #             msg = '{}```{}```'.format(top_name['displayProperties']['description'], tabulate(top_list, tablefmt='plain', colalign=('left', 'left')))
-    #             if len(msg) > 2000:
-    #                 msg_strs = msg.splitlines()
-    #                 msg = ''
-    #                 for line in msg_strs:
-    #                     if len(msg) + len(line) <= 1990:
-    #                         msg = '{}{}\n'.format(msg, line)
-    #                     else:
-    #                         msg = '{}```'.format(msg)
-    #                         await ctx.channel.send(msg)
-    #                         msg = '```{}\n'.format(line)
-    #                 if len(msg) > 0:
-    #                     msg = '{}'.format(msg)
-    #                     await ctx.channel.send(msg)
-    #             else:
-    #                 await ctx.channel.send(msg)
-    #         else:
-    #             await ctx.channel.send(translations['no_data'])
-    #     else:
-    #         await ctx.channel.send(translations['no_clan'], delete_after=10)
-    #     if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #         try:
-    #             await ctx.message.delete()
-    #         except discord.NotFound:
-    #             pass
-
     @commands.slash_command(
         name='top',
         description_localizations={
@@ -318,33 +197,6 @@ class Public(commands.Cog):
         else:
             await ctx.respond(translations['no_clan'])
 
-    # @commands.command()
-    # @commands.guild_only()
-    # async def prefix(self, ctx):
-    #     lang = ctx.bot.guild_lang(ctx.message.guild.id)
-    #     prefixes = ctx.bot.guild_prefix(ctx.guild.id)
-    #     if len(prefixes) > 0:
-    #         msg = '{}\n'.format(ctx.bot.translations[lang]['msg']['prefixes'].format(ctx.message.guild.me.display_name, prefixes[0]))
-    #         prefix_list = ''
-    #         for prefix in prefixes:
-    #             prefix_list = '{} {},'.format(prefix_list, prefix)
-    #         prefix_list = prefix_list[1:-1]
-    #         msg = '{}```{}```'.format(msg, prefix_list)
-    #     else:
-    #         msg = '{}\n'.format(ctx.bot.translations[lang]['msg']['no_prefixes'].format(ctx.message.guild.me.display_name))
-    #     await ctx.message.channel.send(msg)
-    #     if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #         await ctx.message.delete()
-
-    # @commands.command()
-    # async def support(self, ctx):
-    #     if ctx.guild is None:
-    #         lang = 'en'
-    #     else:
-    #         lang = ctx.bot.guild_lang(ctx.guild.id)
-    #     await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'])
-    #     await ctx.channel.send('https://discord.gg/JEbzECp')
-
     @commands.command()
     async def noslash(self, ctx: discord.ext.commands.Context):
         if ctx.guild is None:
@@ -364,46 +216,6 @@ class Public(commands.Cog):
     )
     async def support_sl(self, ctx):
         await ctx.respond('https://discord.gg/JEbzECp')
-
-    # @commands.command()
-    # @commands.guild_only()
-    # async def online(self, ctx):
-    #     ctx.bot.guild_cursor.execute('''SELECT clan_id FROM clans WHERE server_id=?''', (ctx.guild.id,))
-    #     clan_id = ctx.bot.guild_cursor.fetchone()
-    #     lang = ctx.bot.guild_lang(ctx.guild.id)
-    #     translations = ctx.bot.translations[lang]['top']
-    #     await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'])
-    #     if clan_id is None:
-    #         clan_id = [0]
-    #     if clan_id[0] == 0:
-    #         await ctx.channel.send(translations['no_clan'], delete_after=60)
-    #         return
-    #     if len(clan_id) > 0:
-    #         clan_ids = clan_id[0]
-    #         data = await ctx.bot.data.get_online_clan_members(clan_ids, lang)
-    #         if len(data) > 1:
-    #             msg = '```{}```'.format(
-    #                 tabulate(data, tablefmt='simple', colalign=('left', 'left'), headers='firstrow'))
-    #         else:
-    #             msg = '```{}```'.format(
-    #                 tabulate(data, tablefmt='simple', colalign=('left', 'left')))
-    #         if len(msg) > 2000:
-    #             msg_strs = msg.splitlines()
-    #             msg = ''
-    #             for line in msg_strs:
-    #                 if len(msg) + len(line) <= 1990:
-    #                     msg = '{}{}\n'.format(msg, line)
-    #                 else:
-    #                     msg = '{}```'.format(msg)
-    #                     await ctx.channel.send(msg)
-    #                     msg = '```{}\n'.format(line)
-    #             if len(msg) > 0:
-    #                 msg = '{}'.format(msg)
-    #                 await ctx.channel.send(msg)
-    #         else:
-    #             await ctx.channel.send(msg)
-    #     else:
-    #         await ctx.channel.send(translations['no_clan'], delete_after=10)
 
     @commands.slash_command(
         name="online",
--- cogs/serveradmin.py
+++ cogs/serveradmin.py
@@ -104,27 +104,6 @@ class ServerAdmin(commands.Cog):
             await ctx.respond(msg)
         return
 
-    # @commands.command(description='Delete groups that are unavailable or inactive')
-    # async def lfgcleanup(self, ctx, days=0):
-    #     lang = 'en'
-    #     msg = 'Done, removed {} entries.'
-    #     if ctx.guild is None:
-    #         await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'])
-    #         if await ctx.bot.check_ownership(ctx.message, is_silent=False, admin_check=False):
-    #             n = await ctx.bot.lfg_cleanup(days, ctx.guild)
-    #             await ctx.message.channel.send(msg.format(n))
-    #     else:
-    #         lang = ctx.bot.guild_lang(ctx.guild.id)
-    #         await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'], delete_after=60)
-    #         if await ctx.bot.check_ownership(ctx.message, is_silent=False, admin_check=True):
-    #             n = await ctx.bot.lfg_cleanup(days, ctx.guild)
-    #             await ctx.message.channel.send(msg.format(n), delete_after=30)
-    #         if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #             try:
-    #                 await ctx.message.delete()
-    #             except discord.NotFound:
-    #                 pass
-
     @commands.slash_command(name='lfgcleanup',
                             description_localizations={
                                 'ru': "Удалить прошедшие сборы",
@@ -157,27 +136,6 @@ class ServerAdmin(commands.Cog):
                 await ctx.respond(msg.format(n))
             else:
                 await ctx.respond(ctx.bot.translations[lang]['msg']['no_admin'])
-
-    # @commands.command()
-    # @commands.guild_only()
-    # async def regnotifier(self, ctx, upd_type='notifiers'):
-    #     message = ctx.message
-    #     lang = ctx.bot.guild_lang(ctx.guild.id)
-    #     await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'], delete_after=30)
-    #     available_types = ['notifiers', 'seasonal', 'updates']
-    #     notifier_type = 'notifiers'
-    #     if upd_type in available_types:
-    #         notifier_type = upd_type
-    #     if await ctx.bot.check_ownership(message, is_silent=True, admin_check=True):
-    #         ctx.bot.guild_cursor.execute('''INSERT or IGNORE into {} values (?,?)'''.format(notifier_type),
-    #                                     (message.channel.id, message.guild.id))
-    #         ctx.bot.guild_db_sync.commit()
-    #         ctx.bot.get_channels()
-    #         msg = 'Got it, {}'.format(message.author.mention)
-    #         await message.channel.send(msg, delete_after=10)
-    #     if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #         await message.delete()
-    #     return
 
     @commands.slash_command(name='regnotifier',
                             description_localizations={
@@ -224,27 +182,6 @@ class ServerAdmin(commands.Cog):
             await ctx.respond(ctx.bot.translations[lang]['msg']['command_is_done'])
         return
 
-    # @commands.command()
-    # @commands.guild_only()
-    # async def rmnotifier(self, ctx, upd_type='notifiers'):
-    #     message = ctx.message
-    #     lang = ctx.bot.guild_lang(ctx.guild.id)
-    #     await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'], delete_after=30)
-    #     available_types = ['notifiers', 'seasonal', 'updates']
-    #     notifier_type = 'notifiers'
-    #     if upd_type in available_types:
-    #         notifier_type = upd_type
-    #     if await ctx.bot.check_ownership(message, is_silent=True, admin_check=True):
-    #         ctx.bot.guild_cursor.execute('''DELETE FROM {} WHERE channel_id=?'''.format(notifier_type),
-    #                                      (message.channel.id,))
-    #         ctx.bot.guild_db_sync.commit()
-    #         ctx.bot.get_channels()
-    #         msg = 'Got it, {}'.format(message.author.mention)
-    #         await message.channel.send(msg, delete_after=10)
-    #     if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #         await message.delete()
-    #     return
-
     @commands.slash_command(name='rmnotifier',
                             description_localizations={
                                 'ru': "Удалить регистрацию канала для автоматических постов",
@@ -267,28 +204,6 @@ class ServerAdmin(commands.Cog):
             ctx.bot.guild_db_sync.commit()
             ctx.bot.get_channels()
             await ctx.respond(ctx.bot.translations[lang]['msg']['command_is_done'])
-
-    # @commands.command()
-    # @commands.guild_only()
-    # async def setlang(self, ctx, lang):
-    #     message = ctx.message
-    #     if lang.lower() not in ctx.bot.langs:
-    #         msg = 'The language you\'ve entered (`{}`) is not available. Available languages are `{}`.'.format(
-    #             lang, str(ctx.bot.langs).replace('[', '').replace(']', '').replace('\'', ''))
-    #         await message.channel.send(msg)
-    #         return
-    #     if await ctx.bot.check_ownership(message, is_silent=True, admin_check=True):
-    #         ctx.bot.guild_cursor.execute('''UPDATE language SET lang=? WHERE server_id=?''',
-    #                                      (lang.lower(), ctx.message.guild.id))
-    #         ctx.bot.guild_db_sync.commit()
-    #     await ctx.channel.send(ctx.bot.translations[lang.lower()]['msg']['deprecation_warning'], delete_after=30)
-    #     msg = 'Got it, {}'.format(message.author.mention)
-    #     if ctx.guild.me.guild_permissions.change_nickname:
-    #         await ctx.guild.me.edit(nick=ctx.bot.translations[lang.lower()]['nick'], reason='language change')
-    #     await message.channel.send(msg, delete_after=10)
-    #     if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #         await message.delete()
-    #     return
 
     @commands.slash_command(
         name='setlang',
@@ -319,48 +234,6 @@ class ServerAdmin(commands.Cog):
             if ctx.guild.me.guild_permissions.change_nickname:
                 await ctx.guild.me.edit(nick=ctx.bot.translations[args[0].lower()]['nick'], reason='language change')
         await ctx.interaction.edit_original_message(content=msg, view=None)
-
-    # @commands.command()
-    # @commands.guild_only()
-    # async def setclan(self, ctx, clan_id, *args):
-    #     lang = ctx.bot.guild_lang(ctx.message.guild.id)
-    #     await ctx.channel.send(ctx.bot.translations[lang]['msg']['deprecation_warning'], delete_after=60)
-    #     try:
-    #         url = 'https://www.bungie.net/Platform/GroupV2/{}/'.format(int(clan_id))
-    #     except ValueError:
-    #         for arg in args:
-    #             clan_id = '{} {}'.format(clan_id, arg)
-    #         url = 'https://www.bungie.net/Platform/GroupV2/Name/{}/1/'.format(clan_id)
-    #     clan_resp = await ctx.bot.data.get_bungie_json('clan'.format(clan_id), url, string='clan {}'.format(clan_id), change_msg=False)
-    #     clan_json = clan_resp
-    #     try:
-    #         code = clan_json['ErrorCode']
-    #     except KeyError:
-    #         code = 0
-    #     except TypeError:
-    #         await ctx.channel.send('{}: {}'.format(clan_id, ctx.bot.translations[lang]['msg']['clan_search_error']), delete_after=60)
-    #         if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #             await ctx.message.delete()
-    #         return
-    #     if code == 1:
-    #         await ctx.channel.send('{}\nid: {}'.format(clan_json['Response']['detail']['name'],
-    #                                                    clan_json['Response']['detail']['groupId']), delete_after=60)
-    #         if await ctx.bot.check_ownership(ctx.message, is_silent=True, admin_check=True):
-    #             ctx.bot.guild_cursor.execute('''UPDATE clans SET clan_name=?, clan_id=? WHERE server_id=?''',
-    #                                          (clan_json['Response']['detail']['name'],
-    #                                           clan_json['Response']['detail']['groupId'], ctx.guild.id))
-    #             ctx.bot.guild_db_sync.commit()
-    #             if ctx.guild.me.guild_permissions.change_nickname:
-    #                 try:
-    #                     await ctx.guild.me.edit(
-    #                         nick='{}bot'.format(clan_json['Response']['detail']['clanInfo']['clanCallsign']),
-    #                         reason='clan setup')
-    #                 except KeyError:
-    #                     pass
-    #     else:
-    #         await ctx.channel.send('{}: {}'.format(clan_id, clan_json['Message']), delete_after=60)
-    #     if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #         await ctx.message.delete()
 
     @commands.slash_command(name='setclan',
                             description_localizations={
@@ -546,21 +419,6 @@ class ServerAdmin(commands.Cog):
         await ctx.bot.force_update(args, get=get, channels=channels, forceget=get)
         await ctx.interaction.edit_original_message(content=ctx.bot.translations[lang]['msg']['command_is_done'], view=None)
         return
-
-    # @commands.command()
-    # @commands.guild_only()
-    # async def setprefix(self, ctx, prefix, *prefixes):
-    #     prefix = [prefix, *prefixes]
-    #     if await ctx.bot.check_ownership(ctx.message, is_silent=True, admin_check=True):
-    #         if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-    #             await ctx.message.delete()
-    #         if prefix[0].lower() == 'none':
-    #             prefix = []
-    #         ctx.bot.guild_cursor.execute('''UPDATE prefixes SET prefix=? WHERE server_id=?''',
-    #                                      (str(list(prefix)), ctx.message.guild.id))
-    #         ctx.bot.guild_db_sync.commit()
-    #         msg = 'Got it, {}'.format(ctx.message.author.mention)
-    #         await ctx.message.channel.send(msg, delete_after=10)
 
     # @commands.slash_command(
     #     description='Register the channel as a place for LFG posts',
