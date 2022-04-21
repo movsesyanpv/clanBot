82ea849dbb294cd0692f556c3b12cb87e415cef5
movsesyanpv <movsesyanpv1995@live.com>
2022-04-21 22:54:12 +0300

    Removed mentions of legacy commands

--- main.py
+++ main.py
@@ -323,8 +323,7 @@ class ClanBot(commands.Bot):
                                               'To set up automatic Destiny 2 information updates use `/regnotifier` or `/autopost start` command in the channel you want me to post to.\n'
                                               'Please set my language for the guild with `/setlang`, sent in one of the guild\'s chats. Right now it\'s `en`. Available languages are `{}`.\n'
                                               'To use `/top` command you\'ll have to set up a D2 clan with the `/setclan` command.\n'
-                                              'Feel free to ask for help at my Discord Server: https://discord.gg/JEbzECp\n'
-                                              'P.S. There are legacy commands available, you can use `@{} help` to get that list, but they are deprecated.'.
+                                              'Feel free to ask for help at my Discord Server: https://discord.gg/JEbzECp'.
                                               format(str(self.langs).replace('[', '').replace(']', '').replace('\'', ''), self.user.name))
         except AttributeError:
             pass
