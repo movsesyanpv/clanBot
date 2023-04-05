45d8db3da1c3e01fe0f8d05b80f698fe874ec0b1
movsesyanpv <movsesyanpv1995@live.com>
2023-04-05 22:06:52 +0300

    Added some rate limiting code

--- destiny2data.py
+++ destiny2data.py
@@ -24,6 +24,32 @@ from typing import Optional, Union, List
 from lstorations import lost_sector_order, loot_order
 
 
+class Limiter:
+    def __init__(self, calls_limit: int = 5, period: int = 1):
+        self.calls_limit = calls_limit
+        self.period = period
+        self.semaphore = asyncio.Semaphore(calls_limit)
+        self.requests_finish_time = []
+
+    async def sleep(self):
+        if len(self.requests_finish_time) >= self.calls_limit:
+            sleep_before = self.requests_finish_time.pop(0)
+            if sleep_before >= time.monotonic():
+                await asyncio.sleep(sleep_before - time.monotonic())
+
+    def __call__(self, func):
+        async def wrapper(*args, **kwargs):
+
+            async with self.semaphore:
+                await self.sleep()
+                res = await func(*args, **kwargs)
+                self.requests_finish_time.append(time.monotonic() + self.period)
+
+            return res
+
+        return wrapper
+
+
 class RunThread(threading.Thread):
     def __init__(self, func, args, kwargs):
         self.func = func
@@ -283,6 +309,7 @@ class D2data:
                               string: Optional[str] = None, change_msg: bool = True, is_get: bool = True,
                               body: Optional[dict] = None) -> Union[dict, None]:
 
+        @Limiter(calls_limit=20, period=1)
         async def request(url, params, headers, is_get, json=None):
             if is_get:
                 resp = await self.session.get(url, params=params, headers=headers)
@@ -321,7 +348,7 @@ class D2data:
                     self.data[locale][name] = self.data[locale]['api_is_down']
             resp.close()
             return False
-        print('getting {} {}'.format(string, lang_str))
+        print('getting fresh {} {}'.format(string, lang_str))
         curr_try = 2
         while resp_code in self.wait_codes and curr_try <= self.max_retries:
             print('{}, attempt {}'.format(resp_code, curr_try))
